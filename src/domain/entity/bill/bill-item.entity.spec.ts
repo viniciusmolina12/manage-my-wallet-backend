a89b3f5df861af18de98832ec3c8d8b4d7336041
Vinicius Molina <vinicius.molina@einstein.br>
2023-01-28 16:51:17 -0300

    test: add bill item tests

---
+++ src/domain/entity/bill/bill-item.entity.spec.ts
@@ -0,0 +1,50 @@
+import BillItem from "./bill-item.entity"
+
+describe('BillItem entity unit tests', () => {
+    it('should throw a error if id is empty', () => {
+        expect(() => {
+            const billItem = new BillItem('', 'itemId', 1, 1);
+        }).toThrowError('Id is required');
+    })
+
+    it('should throw a error if itemId is empty', () => {
+        expect(() => {
+            const billItem = new BillItem('1', '', 1, 1);
+        }).toThrowError('ItemId is required');
+    })
+
+    it('should throw a error if price is less or equal 0', () => {
+        expect(() => {
+            const billItem = new BillItem('1', 'itemId', -1, 1);
+        }).toThrowError('Price must be greater than 0');
+    })
+
+    it('should throw a error if quantity is less or equal 0', () => {
+        expect(() => {
+            const billItem = new BillItem('1', 'itemId', 1, -1);
+        }).toThrowError('Quantity must be greater than 0');
+    })
+
+    it('should change price', () => {
+        const billItem = new BillItem('1', 'itemId', 100, 1);
+        expect(billItem.price).toBe(100);
+        billItem.changePrice(250);
+        expect(billItem.price).toBe(250);
+    })
+
+    it('should change quantity', () => {
+        const billItem = new BillItem('1', 'itemId', 100, 2);
+        expect(billItem.quantity).toBe(2);
+        billItem.changeQuantity(5);
+        expect(billItem.quantity).toBe(5);
+    })
+
+    it('should calculate total', () => {
+        const billItem = new BillItem('1', 'itemId', 150, 2);
+        expect(billItem.total).toBe(300)
+        billItem.changeQuantity(4);
+        expect(billItem.total).toBe(600);
+        billItem.changePrice(100);
+        expect(billItem.total).toBe(400);
+    })
+})
