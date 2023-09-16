24f0088e9ef7986a2b007dfb4d272b312ad2ca4f
Vinicius Molina <vinicius.molina@einstein.br>
2023-09-16 12:52:45 -0300

    feat: add update item usecase

---
+++ src/core/usecases/item/update/update.usecase.ts
@@ -0,0 +1,22 @@
+import Item from "../../../domain/item/entity/item.entity";
+import { ItemRepository } from "../../../domain/item/repository/item.repository";
+import { InputUpdateItemDto, OutputUpdateItemDto } from "./update.item.dto";
+
+export default class UpdateItemUseCase {
+
+    constructor(private itemRepository: ItemRepository) { 
+        this.itemRepository = itemRepository;
+    }
+
+    async execute(input: InputUpdateItemDto): Promise<OutputUpdateItemDto> {
+        const item = await this.itemRepository.find(input.id)
+        if(!item) throw new Error('Item not found');
+        const updatedItem = new Item(input.id, input.name, input.categoryId, input.description);
+        await this.itemRepository.update(updatedItem)
+        return {
+            categoryId: updatedItem.categoryId,
+            description: updatedItem.description,
+            name: updatedItem.name
+        };
+    }
+}
