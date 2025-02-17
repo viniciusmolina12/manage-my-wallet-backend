a55761146654d9ad6ea292031c1cd474bea0f6bc
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-02-17 20:38:53 -0300

    feat: add node mailer implementation

---
+++ src/infrastructure/mailer/node-mailer.ts
@@ -0,0 +1,17 @@
+import nodemailer, { Transporter } from "nodemailer";
+import Mailer from "@core/domain/interfaces/mailer.interface";
+
+export class NodeMailerMailer implements Mailer {
+    private readonly transporter: Transporter
+    constructor(config: { host: string, port: number, auth?: { user: string, pass: string} }) {
+        this.transporter = nodemailer.createTransport(config);
+    }
+    async sendMail(data: { to: string; from: string; subject: string; content: string; }): Promise<void> {
+        await this.transporter.sendMail({
+            to: data.to,
+            from: data.from,
+            subject: data.subject,
+            text: data.content
+        })
+    }
+}
