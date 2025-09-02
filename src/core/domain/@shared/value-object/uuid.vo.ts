63a13566b5ee0f4c664236d34e60e22d895bba39
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-09-02 20:16:25 -0300

    feat: add value object interface and uuid vo

---
+++ src/core/domain/@shared/value-object/uuid.vo.ts
@@ -0,0 +1,29 @@
+import { ValueObject } from '../value-object.interface';
+import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
+
+export class Uuid extends ValueObject {
+   readonly id: string;
+   constructor(id?: string) {
+      super();
+      this.id = id || uuidv4();
+      this.validate();
+   }
+
+   private validate() {
+      const isValid = uuidValidate(this.id);
+      if (!isValid) {
+         throw new InvalidUuidError();
+      }
+   }
+
+   toString() {
+      return this.id;
+   }
+}
+
+export class InvalidUuidError extends Error {
+   constructor(message?: string) {
+      super(message || 'Invalid UUID');
+      this.name = 'InvalidUuidError';
+   }
+}
