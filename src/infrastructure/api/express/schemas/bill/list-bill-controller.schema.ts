efab2272153c9c939f173e2f1f8fd4461fde8dbb
Vinicius Molina <viniciusgranmolina@gmail.com>
2025-10-20 19:21:43 -0300

    fix: list bill controller schema

--- src/infrastructure/api/express/schemas/bill/list-bill-controller.schema.ts
+++ src/infrastructure/api/express/schemas/bill/list-bill-controller.schema.ts
@@ -4,6 +4,13 @@ export const listBillControllerSchema = z.object({
    userId: z.string().min(1),
    page: z.number().min(1),
    perPage: z.number().min(1),
-   order: z.string().min(1),
-   search: z.string().min(1),
+   order: z.string().min(1).optional(),
+   search: z
+      .object({
+         name: z.string().optional(),
+         vendorId: z.string().optional(),
+         startDate: z.coerce.date().optional(),
+         endDate: z.coerce.date().optional(),
+      })
+      .optional(),
 });
--- src/infrastructure/api/express/schemas/user/create-user-controller.schema.ts
+++ src/infrastructure/api/express/schemas/user/create-user-controller.schema.ts
@@ -5,7 +5,4 @@ export const createUserControllerSchema = z.object({
    email: z.string().email(),
    password: z.string().min(6),
    confirmPassword: z.string().min(6),
-}).refine((data) => data.password === data.confirmPassword, {
-   message: "Passwords don't match",
-   path: ["confirmPassword"],
 });
